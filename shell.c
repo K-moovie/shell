34d594b1f445cb74f9d1d6edf24ff374aca74a98
Kang Ho Dong <stardustsw8947@gmail.com>
2020-11-12 21:45:41 +0900

    Add Signal Functions

--- shell.c
+++ shell.c
@@ -7,16 +7,35 @@ author: github_K-moovie, github_SWKANG0525
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+pid_t pid;
 int getargs(char *cmd, char **argv);
+void SIGINT_Handler(int signo);
+void SIGQUIT_Handler(int signo);
 
+void SIGQUIT_Handler(int signo) {
+    printf("\n");
+    exit(1);
+}
+
+void SIGINT_Handler(int signo)
+{   
+    if(pid == 0) {
+        printf("\n");
+        exit(1);
+    }
+}
 int main()
 {
+    signal(SIGINT, SIGINT_Handler);
+    signal(SIGTSTP, SIGQUIT_Handler);
     char buf[256];
     char *argv[50];
     int narg;
-    pid_t pid;
+    
     while (1)
     {
+
         printf("shell> ");
         gets(buf);
         //  Q1. exit 입력 시 프로그램 종료.
@@ -26,10 +45,21 @@ int main()
 
         narg = getargs(buf, argv);
         pid = fork();
-        if (pid == 0)
-            execvp(argv[0], argv);
-        else if (pid > 0)
+        if (pid == 0) {
+
+            
+            // execvp(argv[0], argv);
+            while(1)
+            {
+                printf( "forum.falinux.com\n");
+                sleep(1);
+            }
+
+        }
+        else if (pid > 0) {
             wait((int *)0);
+
+        }
         else
             perror("fork failed");
     }
